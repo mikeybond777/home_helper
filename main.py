d8fc6036960ceb5783d117b81ac93a97ef02cccb
Michael Bond <michaelbond@nexusstudios.com>
2025-07-27 17:20:10 +0100

    First commit, create prep images file and copy image classifier from other proj

---
+++ main.py
@@ -0,0 +1,230 @@
+from PIL import Image
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import transforms
+import timm
+
+from tqdm.auto import tqdm
+
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+TEST_IMAGE_PATH = "./archive/test/angie/002.jpg"
+
+
+# Create the classifier model.
+class SimpleImageClassifier(nn.Module):
+    def __init__(self, num_classes=53):
+        super(SimpleImageClassifier, self).__init__()
+
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
+        # Remove the last layer of model (to replace with our own)
+        self.features = nn.Sequential(*list(self.base_model.children())[:-1])
+
+        enet_out_size = 1280
+        self.classifier = nn.Linear(enet_out_size, num_classes)
+
+    def forward(self, x):
+        # Connect these parts and return the output.
+        x = self.features(x)
+        output = self.classifier(x)
+        return output
+
+
+class FacesDataset(Dataset):
+    def __init__(self, data_dir, transform=None):
+        super(FacesDataset, self).__init__()
+        self.data = ImageFolder(data_dir, transform=transform)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+    @property
+    def classes(self):
+        return self.data.classes
+
+
+def preprocess_image(image_path, transform):
+    '''Load and preprocess the image.'''
+    image = Image.open(image_path).convert("RGB")
+    return image, transform(image).unsqueeze(0)
+
+
+def predict(model, image_tensor, device):
+    '''Predict using the model.'''
+    model.eval()
+    with torch.no_grad():
+        image_tensor = image_tensor.to(device)
+        outputs = model(image_tensor)
+        probabilities = torch.nn.functional.softmax(outputs, dim=1)
+    return probabilities.cpu().numpy().flatten()
+
+
+def get_images_to_tensor_transform():
+    transform = transforms.Compose([
+        transforms.Resize((128, 128)),
+        transforms.ToTensor(),
+    ])
+
+    return transform
+
+
+def test_model(model, class_names):
+    # Top numbers to print.
+    top_n = 5
+    test_image = TEST_IMAGE_PATH
+
+    original_image, image_tensor = preprocess_image(test_image, get_images_to_tensor_transform())
+
+    # Total of all probabilities is 1.
+    probabilities = predict(model, image_tensor, DEVICE)
+
+    # Pair with class names
+    result = dict(zip(class_names, probabilities))
+
+    # Sort & print top N
+    top_indices = probabilities.argsort()[-top_n:][::-1]
+    print("Top Predictions:")
+    for idx in top_indices:
+        print(f"{class_names[idx]}: {probabilities[idx]:.4f}")
+
+    return result
+
+
+def loss_function(model, images, labels):
+    '''Do training and validation loss calculation.'''
+
+    example_out = model(images)
+
+    # Criterion allows you to calculate loss by measuring outputs of model against labels.
+    criterion = nn.CrossEntropyLoss()
+
+    # Adam is one of the best optimizers.
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    criterion(example_out, labels)
+    transform = get_images_to_tensor_transform()
+
+    # Create data loaders for training, validation and testing.
+    train_folder = './archive/train/'
+    valid_folder = './archive/valid/'
+    test_folder = './archive/test/'
+
+    train_dataset = FacesDataset(train_folder, transform=transform)
+    val_dataset = FacesDataset(valid_folder, transform=transform)
+    test_dataset = FacesDataset(test_folder, transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+    test_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+
+    # How many times the model sees all images in the training data set and does forward back pass.
+    num_epochs = 20
+    train_losses, val_losses = [], []
+
+    # Set pytorch to use the gpu rather than cpu.
+
+    model.to(DEVICE)
+    for epoch in range(num_epochs):
+        model.train()
+
+        # Keeping track of loss.
+        running_loss = 0.0
+
+        # Wrap the loop in a tqdm do get progress bars :)
+        for images, labels in tqdm(train_loader, desc='Training Loop', leave=True):
+            # Move inputs and labels to the GPU
+            images, labels = images.to(DEVICE), labels.to(DEVICE)
+            # Resets to zero gradients of all model params optimizer is tracking.
+            optimizer.zero_grad()
+
+            # Run the images through the model.
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            # Loss and optimizer are linked to the same graph.
+            # Do back propagation to calculate new grads, then apply them to the model with optimizer step.
+            # Applying updated gradients means tweaking parameters in opposite direction of their gradient.
+            # As gradient of each param points in direction of the steepest increase in loss.
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item() * labels.size(0)
+        train_loss = running_loss / len(train_loader.dataset)
+        train_losses.append(train_loss)
+
+        # Set model to the validation phase.
+        model.eval()
+        running_loss = 0.0
+
+        # Dont track gradients or build computational graph (only required when doing back prop).
+        with torch.no_grad():
+            for images, labels in tqdm(val_loader, desc='Validation Loop'):
+                # Move inputs and labels to the GPU
+                images, labels = images.to(DEVICE), labels.to(DEVICE)
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                running_loss += loss.item() * labels.size(0)
+            val_loss = running_loss / len(val_loader.dataset)
+            val_losses.append(val_loss)
+        tqdm.write(f"\nEpoch {epoch + 1}/{num_epochs} - Train loss: {train_loss}, Validation loss: {val_loss}")
+
+    return model
+
+
+def train():
+    '''Train the model with the training dataset.'''
+
+    # Need to transform PIL image data into tensors of the same size, so we create this transform to plug into class.
+    transform = get_images_to_tensor_transform()
+
+    dataset = FacesDataset(data_dir='./archive/train', transform=transform)
+
+    # Data loader to parallelize loading of images in dataset.
+    # Batch size is how many examples to pull each time we iterate.
+    # Shuffle loads random data when iterating (usually done when training).
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+    for images, labels in dataloader:
+        break
+
+    # Should print a tensor [<batch size>, channels, image width, image height]
+    # Labels is a one d array of length num labels.
+    print(images.shape)
+
+    model = SimpleImageClassifier(num_classes=5)
+    print(model(images))
+
+    # Train the model and print losses.
+    model = loss_function(model, images, labels)
+
+    torch.save(model.state_dict(), './model/model.pth')
+
+
+def test():
+    '''Test the model on the specified test image.'''
+
+    # Create the dataset just to get the classes.
+    dataset = FacesDataset(data_dir='./archive/train', transform=get_images_to_tensor_transform())
+
+    # Recreate the model on the GPU.
+    model = SimpleImageClassifier(num_classes=5)
+    state_dict = torch.load('./model/model.pth', map_location=DEVICE, weights_only=True)
+    model.load_state_dict(state_dict)
+    model.to(DEVICE)
+
+    # Set the model to evaluation mode (important if using batch norm/dropout layers)
+    model.eval()
+
+    # Test the model with images.
+    test_model(model, dataset.classes)
+
+
+#train()
+test()
---
+++ prep_images_for_training.py
@@ -0,0 +1,256 @@
+import os
+import shutil
+
+import face_recognition
+from PIL import Image, ExifTags
+import pillow_heif
+
+
+SOURCE_IMAGES = "./image_processing/raw"
+KEY_FACES_DIR = "./image_processing/key_faces"
+OUTPUT_DIR = "./image_processing/output"
+DATA_INPUT_DIR = "./archive"
+
+TRAINING = 'train'
+VALIDATION = 'valid'
+TESTING = 'test'
+
+
+def crop_to_key_face(source_image_path, key_face_image_path, output_image_path, padding=100):
+    try:
+        # Load the images
+        source_image = face_recognition.load_image_file(source_image_path)
+        key_face_image = face_recognition.load_image_file(key_face_image_path)
+
+        # Find all face locations and encodings in the source image
+        source_face_locations = face_recognition.face_locations(source_image)
+        source_face_encodings = face_recognition.face_encodings(source_image, source_face_locations)
+
+        # Get the encoding of the key face
+        # We assume the key face image contains only one face
+        key_face_encoding = face_recognition.face_encodings(key_face_image)[0]
+
+        print(f"Found {len(source_face_encodings)} face(s) in the source image.")
+        print("Searching for the key face...")
+
+        # Find the matching face in the source image
+        match_found = False
+        for i, source_face_encoding in enumerate(source_face_encodings):
+            # Compare faces
+            matches = face_recognition.compare_faces([key_face_encoding], source_face_encoding)
+
+            if matches[0]:
+                print("Match found!")
+                # Get the coordinates of the matched face
+                top, right, bottom, left = source_face_locations[i]
+
+                # Open the original image using Pillow to crop
+                pil_image = Image.open(source_image_path)
+
+                # Define the crop box with padding
+                crop_left = max(0, left - padding)
+                crop_top = max(0, top - padding)
+                crop_right = min(pil_image.width, right + padding)
+                crop_bottom = min(pil_image.height, bottom + padding)
+
+                # Crop the image
+                cropped_image = pil_image.crop((crop_left, crop_top, crop_right, crop_bottom))
+
+                # Save the cropped image
+                cropped_image.save(output_image_path)
+                print(f"Cropped image saved to {output_image_path}")
+                match_found = True
+                break  # Exit after finding the first match
+
+        if not match_found:
+            print("Could not find the key face in the source image.")
+
+    except IndexError:
+        print("Error: Could not find any face in the key face image. Please use a clear photo.")
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
+
+
+def list_all_file_paths(directory):
+    file_paths = []
+
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            full_path = os.path.join(root, file)
+            file_paths.append(full_path)
+
+    return file_paths
+
+
+def resize_image_to_dimensions(input_path, width=224, height=224):
+    if not os.path.exists(input_path):
+        print(f"File not found: {input_path}")
+        return
+
+    try:
+        image = Image.open(input_path)
+
+        # Resize to fixed width and height
+        resized = image.resize((width, height), Image.LANCZOS)
+
+        # Save with modified name
+        base, ext = os.path.splitext(input_path)
+        output_path = f"{base}_resized{ext}"
+        resized.save(input_path, "JPEG", quality=90)
+    except Exception as e:
+        print("Unable to resize image: %s" % e)
+
+
+def apply_exif_orientation(image):
+    try:
+        exif = image._getexif()
+        if exif is not None:
+            for orientation_tag in ExifTags.TAGS.keys():
+                if ExifTags.TAGS[orientation_tag] == 'Orientation':
+                    break
+            orientation = exif.get(orientation_tag, None)
+
+            if orientation == 3:
+                image = image.rotate(180, expand=True)
+            elif orientation == 6:
+                image = image.rotate(270, expand=True)
+            elif orientation == 8:
+                image = image.rotate(90, expand=True)
+    except Exception as e:
+        print(f"Warning: Could not apply EXIF orientation. {e}")
+    return image
+
+
+def convert_image_to_jpg(input_path):
+    """Converts a single image file (e.g., HEIC, PNG, TIFF) to a JPG file."""
+
+    # Verify the input file path exists
+    if not os.path.exists(input_path):
+        print(f"Error: File not found at '{input_path}'")
+        return
+
+    # Construct the output path by replacing the extension
+    base_path = os.path.splitext(input_path)[0]
+    jpg_path = base_path + ".jpg"
+
+    if jpg_path.lower().replace('\\', '/') == input_path.lower().replace('\\', '/'):
+        return
+
+    print(f"Converting '{os.path.basename(input_path)}' to '{os.path.basename(jpg_path)}'...")
+
+    try:
+        # Register the HEIF opener with Pillow.
+        # This allows Pillow to open .heic files in addition to its
+        # standard supported formats like PNG, BMP, etc.
+        pillow_heif.register_heif_opener()
+
+        # Open and discard alpha.
+        image = Image.open(input_path)
+        # Avoid unintentional rotations.
+        image = apply_exif_orientation(image)
+        if image.mode in ("RGBA", "P"):
+            image = image.convert("RGB")
+
+        # 'quality=90' is a good balance between file size and quality.
+        image.save(jpg_path, "jpeg", quality=90)
+        print("Conversion successful!")
+        if jpg_path.lower().replace('\\', '/') != input_path.lower().replace('\\', '/') and os.path.exists(jpg_path):
+            os.remove(input_path)
+
+    except FileNotFoundError:
+        print(f"Error: The file '{input_path}' was not found.")
+    except Exception as e:
+        print(f"An error occurred. The file might not be a supported image format. Details: {e}")
+
+
+def convert_images_to_jpg(images_dir):
+    '''Convert all images in raw data to jpg.'''
+
+    all_image_paths = list_all_file_paths(images_dir)
+    for image_path in all_image_paths:
+        convert_image_to_jpg(image_path)
+
+
+def analyse_crop_images():
+    for key_image in os.listdir(KEY_FACES_DIR):
+        key_image_full_path = os.path.abspath('%s/%s' % (KEY_FACES_DIR, key_image))
+        face_name = os.path.splitext(key_image)[0]
+
+        if face_name in ['angie', 'bond', 'cameron']:
+            continue
+
+        # Ensure key image output dir exists.
+        output_dir = os.path.abspath('%s/%s' % (OUTPUT_DIR, face_name))
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir, exist_ok=True)
+
+        source_image_dir = os.path.abspath('%s/%s' % (SOURCE_IMAGES, face_name))
+        if not os.path.exists(source_image_dir):
+            print(f"WARNING, no face images found for {face_name}!")
+            continue
+        for source_image in os.listdir(source_image_dir):
+            crop_to_key_face('%s/%s' % (source_image_dir, source_image), key_image_full_path, '%s/%s' % (output_dir, source_image))
+
+
+def reformat_images(images_dir):
+    all_file_paths = list_all_file_paths(images_dir)
+    for file_path in all_file_paths:
+        resize_image_to_dimensions(file_path)
+
+
+def _split_data_training_validation_test(image_files_dir):
+    image_files = ['%s/%s' % (image_files_dir, image_name) for image_name in os.listdir(image_files_dir)]
+    image_files = [image_file for image_file in image_files if os.path.isfile(image_file)]
+
+    total = len(image_files)
+
+    # Calculate initial sizes
+    two_thirds = (2 * total) // 3
+    remaining = total - two_thirds
+    one_sixth = remaining // 2
+
+    # Assign leftovers (e.g. when total isn't divisible cleanly)
+    remainder = total - (two_thirds + one_sixth * 2)
+    data_input = {
+        TRAINING: image_files[:two_thirds + remainder],
+        VALIDATION: image_files[two_thirds + remainder:two_thirds + remainder + one_sixth],
+        TESTING: image_files[two_thirds + remainder + one_sixth:]
+    }
+
+    return data_input
+
+def copy_images_for_model_training(output_dir, padding=3):
+    '''Copy images from cropped output folder to directories for training.'''
+
+    for category_name in os.listdir(output_dir):
+        full_output_path = '%s/%s' % (output_dir, category_name)
+        print(full_output_path)
+        if not os.path.isdir(full_output_path):
+            continue
+
+        # Find different types of data based on collected data.
+        data_input = _split_data_training_validation_test(full_output_path)
+
+        print(data_input)
+
+        # Copy the files to relevant locations for the model to be trained with.
+        for data_type, image_files in data_input.items():
+            data_type_path = '%s/%s/%s' % (DATA_INPUT_DIR, data_type, category_name)
+            if not os.path.exists(data_type_path):
+                os.makedirs(data_type_path, exist_ok=True)
+            for i, image_file_path in enumerate(image_files):
+                number = i+1
+                ext = os.path.splitext(image_file_path)[1]
+                image_file_padded_name = f"{number:0{padding}d}{ext}"
+                new_image_path = '%s/%s' % (data_type_path, image_file_padded_name)
+                shutil.copy2(image_file_path, new_image_path)
+
+if __name__ == "__main__":
+    #convert_images_to_jpg(os.path.abspath(SOURCE_IMAGES))
+    #convert_images_to_jpg(os.path.abspath(KEY_FACES_DIR))
+    #analyse_crop_images()
+    #reformat_images(os.path.abspath(OUTPUT_DIR))
+
+    copy_images_for_model_training(os.path.abspath(OUTPUT_DIR).replace('\\', '/'))
+
+
